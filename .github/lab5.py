4c2e9ecdbe924c20a1d3258a0854cb89002855e0
baseb <cmplowman@loyola.edu>
2021-10-23 18:51:37 -0400

    Push 4

--- .github/lab5.py
+++ .github/lab5.py
@@ -2,10 +2,10 @@
 # CS151, Dr. Rajeev
 # Date: October 21, 2021
 # Lab 5
-# Program Inputs: Room dimensions (length + width) for each room
-# Program Outputs: Cost of floors redone for each room
+# Program Inputs: Room dimensions (length + width) for each room.
+# Program Outputs: Cost of floors redone for each room.
 
-# ask user to enter a room 1-5
+# ask user to enter a room 1-5.
 
-# condtionals: outside if statement for room # and cost calculation on the inside
-# NOTE: conditionals are the same for all rooms
+# condtionals: outside if statement for room # and cost calculation on the inside.
+# NOTE: conditionals are the same for all rooms.
